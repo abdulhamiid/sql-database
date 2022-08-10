273e76f748ea1d1129fdceecd2df4b65f5392f36
abdulhamiid <adioabdulhamid@yahoo.com>
2022-08-10 23:47:08 +0100

    Add transactions as requested by reviewer

--- queries.sql
+++ queries.sql
@@ -9,6 +9,26 @@ SELECT * from animals WHERE neutered = 't';
 SELECT * from animals WHERE name <> 'Gabumon';
 SELECT * from animals WHERE weight_kg BETWEEN 10.4 AND 17.3;
 
+-- Transactions
+BEGIN;
+UPDATE animals SET species = 'unspecified';
+ROLLBACK;
+UPDATE animals SET species = 'digimon' WHERE name LIKE '%mon%';
+UPDATE animals SET species = 'pokemon' WHERE species = '';
+COMMIT;
+
+BEGIN;
+DELETE FROM animals;
+ROLLBACK;
+
+BEGIN;
+DELETE FROM animals WHERE date_of_birth > '2022-01-01';
+SAVEPONT SP1;
+UPDATE animals SET weight_kg = weight_kg * -1;
+ROLLBACK TO SP1;
+UPDATE animals SET weight_kg = weight_kg * -1 WHERE weight_kg < 0;
+COMMIT;
+
 -- new queries
 
 SELECT COUNT(*) FROM animals;
